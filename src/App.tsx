ad6b31c26667c2527538c9700494d8adbacb1f28
Tapesh-Vashisth <tapeshvashisth789@gmail.com>
2023-11-10 09:42:35 +0530

    routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
 import { useState } from 'react'
+import { Routes, Route } from "react-router-dom";
+import Login from './pages/login';
+import Signup from './pages/signup';
+import ProtectedPages from './components/ProtectedPages';
 
 function App() {
   return (
     <>
+      <Routes>
+        <Route path='/' element={<ProtectedPages />}>
+          <Route path='login' element={<Login />} />
+          <Route path='signup' element={<Signup />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Signup.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// first name 
+// last name 
+// emailid 
+// mobile number 
+// password 
+function Signup() {
+  return (
+    <div>Signup</div>
+  )
+}
+
+export default Signup
